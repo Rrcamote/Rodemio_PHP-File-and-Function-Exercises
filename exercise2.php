a78b31938fad196be1e47bd1a60071718a6ce021
RRjaa <77971498+Rrcamote@users.noreply.github.com>
2021-04-18 17:07:05 +0800

    save changes

--- exercise2.php
+++ exercise2.php
@@ -9,10 +9,9 @@
     <title>Document</title>
     <style>
 body {
-    background-color: #ff7700;
-background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1600 900'%3E%3Cpolygon fill='%23cc0000' points='957 450 539 900 1396 900'/%3E%3Cpolygon fill='%23aa0000' points='957 450 872.9 900 1396 900'/%3E%3Cpolygon fill='%23d6002b' points='-60 900 398 662 816 900'/%3E%3Cpolygon fill='%23b10022' points='337 900 398 662 816 900'/%3E%3Cpolygon fill='%23d9004b' points='1203 546 1552 900 876 900'/%3E%3Cpolygon fill='%23b2003d' points='1203 546 1552 900 1162 900'/%3E%3Cpolygon fill='%23d3006c' points='641 695 886 900 367 900'/%3E%3Cpolygon fill='%23ac0057' points='587 900 641 695 886 900'/%3E%3Cpolygon fill='%23c4008c' points='1710 900 1401 632 1096 900'/%3E%3Cpolygon fill='%239e0071' points='1710 900 1401 632 1365 900'/%3E%3Cpolygon fill='%23aa00aa' points='1210 900 971 687 725 900'/%3E%3Cpolygon fill='%23880088' points='943 900 1210 900 971 687'/%3E%3C/svg%3E");
-background-attachment: fixed;
-background-size: cover;
+    background-image: url('https://image.freepik.com/free-vector/wood-background-realistic_107791-102.jpg?1');
+    background-repeat: no-repeat;
+    background-size: cover;
 }
 
 </style>
@@ -30,32 +29,31 @@ background-size: cover;
 <p class="text-white">2.)Create a function that allows you to append a line into a file given the line number</p>
 </div>
 <div class="card-body">
-<input type="submit" name="clear" value="Delete">
 <?php
-if(isset($_POST['clear'])){
-     
-}
-    function existFiles($files_Name){
-        if(file_exists($files_Name)){
-             $file = fopen($files_Name,'a');
-             fwrite($file,"\n This text  will added to the the myText file");
-             fclose($file);
+    function existFile($filename,$fileData,$lineNumber){
+        if(file_exists($filename)){
+            $file = fopen($filename,'a');
+            $lines = file($filename,FILE_IGNORE_NEW_LINES);
+            array_splice( $lines, $lineNumber-1, 0, $fileData );
+            file_put_contents($filename,join("\n",($lines)));
+            fclose($file);
 
+        }else{
+            echo "File does not exist!";
         }
     }
-
-
-    function showFiles($files_Name){
-        if(file_exists($files_Name)){
-            $file = fopen($files_Name,'r');
-            $filesContents = fread($file, filesize($files_Name));    
-            echo "<pre>$filesContents</pre>"; 
+    function readFiles($filename){
+        if(file_exists($filename)){
+            $file = fopen($filename,'r');
+            $contents = fread($file, filesize($filename));   
+            echo "<pre>$contents</pre>";  
             fclose($file);
 
        }
     }
-    showFiles('newTxt.txt');
-    existFiles('newTxt.txt')
+    readFiles('newTxt.txt');
+    existFile('newTxt.txt',"This text will be added",4);
+
 ?>
 </div>
 </div>
